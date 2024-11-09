03d9979282ee8d005cc826b0b91876ea0167a134
GustavoRomero <gusta.romero.gr@gmail.com>
2024-11-09 18:30:49 -0300

    boton aceptar

--- REPOSITORIO/Form1.Designer.vb
+++ REPOSITORIO/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class FRMPRINCIPAL
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
@@ -22,9 +22,30 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.BTNACEPTAR = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'BTNACEPTAR
+        '
+        Me.BTNACEPTAR.Location = New System.Drawing.Point(44, 119)
+        Me.BTNACEPTAR.Name = "BTNACEPTAR"
+        Me.BTNACEPTAR.Size = New System.Drawing.Size(98, 54)
+        Me.BTNACEPTAR.TabIndex = 0
+        Me.BTNACEPTAR.Text = "ACEPTAR"
+        Me.BTNACEPTAR.UseVisualStyleBackColor = True
+        '
+        'FRMPRINCIPAL
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(520, 395)
+        Me.Controls.Add(Me.BTNACEPTAR)
+        Me.Name = "FRMPRINCIPAL"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
+    Friend WithEvents BTNACEPTAR As System.Windows.Forms.Button
 
 End Class
